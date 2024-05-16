eaa7aa68fd3fe15255655693c0c99cdd6a2bae33
Guillermo Chavez <20230075@ricaldone.edu.sv>
2024-05-15 20:58:47 -0600

    Practica

---
+++ app/src/main/java/guillermo/chavez/practica_guillermo_chavez/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package guillermo.chavez.practica_guillermo_chavez.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
